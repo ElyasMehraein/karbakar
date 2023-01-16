c3b803e0fbd7808a22344cb613fea93c32f6e183
ElyasMehraein <60826606+ElyasMehraein@users.noreply.github.com>
2023-01-16 12:36:45 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import PrimarySearchAppBar from "./pages/index/PrimarySearchAppBar"
+import TabPanel from "./pages/index/TabPanel"
+
+
+function App() {
+  return (
+  <PrimarySearchAppBar></PrimarySearchAppBar>
+  
+  , <TabPanel></TabPanel>
+  
+  
+  );
+}
+
+export default App;
