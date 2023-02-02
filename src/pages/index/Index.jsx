759ef5d7d27a9bf4a948aedcc2aae8ad5972f5e7
ElyasMehraein <elyas.mehraein@gmail.com>
2023-02-02 23:31:27 +0330

    have problem changing index tab

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import TabPanel from "./TabPanel";
+import IndexBar from "./indexBar";
 import IndexTab from "./indexTab";
+import { Box } from "@mui/material";
 
 function Index() {
     return (
-        <>
+        <Box>
+            <IndexBar />
             <IndexTab />
-
-            <TabPanel />
-        </>
+        </Box>
     )
 }
 
---
+++ src/pages/index/temporaryComponents/YourReq.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+
+const YourReq = () => {
+    return (
+        <Typography>
+            هر چیزی لازم داری با زدن دکمه درخواست جدید اعلام کن
+
+            هر چقدر کارهای بیشتری از درخواست های دیگران انجام بدی دیگران هم درخواست های بیشتری برای شما انجام خواهند داد
+        </Typography>
+    )
+}
+
+export default YourReq
