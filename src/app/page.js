4b3dac1189994c16b9ba111fcb88fb5ddbf58b96
ElyasMehraein <elyas.mehraein@gmail.com>
2024-01-08 15:04:47 +0330

    create two pages

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,16 @@ import React from "react";
 import Tabs from "@/components/index/Tabs";
 import DrawerAndAppBar from "@/components/index/DrawerAndAppBar";
 import { Box } from "@mui/material";
+import PrimarySearchAppBar from "@/components/index/PrimarySearchAppBar";
+import PersistentDrawerRight from "@/components/index/PersistentDrawerRight";
+
 
 function Index() {
   return (
     <Box>
-      <DrawerAndAppBar />
-      <Tabs />
+      <PrimarySearchAppBar/>
+      <PersistentDrawerRight/>
+      
     </Box>
   );
 }
