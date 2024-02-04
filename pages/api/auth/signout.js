d0fe5cda60018bbf9cb6692461a6f033f0ff5fab
ElyasMehraein <elyas.mehraein@gmail.com>
2024-02-04 18:20:21 +0330

    working on login

--- pages/api/auth/signout.js
+++ pages/api/auth/signout.js
@@ -1,5 +1,12 @@
+import { serialize } from "cookie";
 const handler=()=>{
-    //
+    if (req.method !== "GET") {
+        res.status(422).json({ message: 'request method must be "GET"' })
+    }
+    return res.status(200).setHeader('Set-Cookie', serialize('token', "",{
+     path:"/",
+     maxAge: 60*60*24*7
+    })).status(422).json({ message: 'user loggd oft successfullt'})
 
 }
 export default handler
