a180131eba1679db70d5aa21956c61ab459448f5
ElyasMehraein <elyas.mehraein@gmail.com>
2024-02-09 20:07:23 +0330

    capability of changing avatar and profile cover
    i just add ui buttons to do that so

--- components/editProfile/ProfileImg.jsx
+++ components/editProfile/ProfileImg.jsx
@@ -5,12 +5,17 @@ import IconButton from '@mui/material/IconButton';
 import AddAPhotoOutlinedIcon from '@mui/icons-material/AddAPhotoOutlined';
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import { Box, Button, Icon } from '@mui/material';
+import { grey } from '@mui/material/colors';
+
+const color = grey[900];
 
 export default function Background() {
   return (
-    <div
+    <Box
+      display="flex" alignItems="flex-end" justifyContent="left"
       style={{
-        // use relative position for the parent div
+        // use relative position for the parent Box
+
         position: "relative",
         width: "100vw",
         height: "40vh",
@@ -26,13 +31,12 @@ export default function Background() {
           objectFit: 'cover',
         }}
       />
-      <IconButton sx={{ bgcolor: 'text.primary' }}
-      >
-        <AddAPhotoIcon sx={{ color:'text.disabled',fontSize: 40 }} />
+      <IconButton sx={{ ml: 5, mb: 5, bgcolor: color }}>
+        <AddAPhotoIcon sx={{ color: 'white' }} />
       </IconButton>
 
 
 
-    </div>
+    </Box    >
   )
 }
--- components/editProfile/UserAvatar.jsx
+++ components/editProfile/UserAvatar.jsx
@@ -2,57 +2,38 @@ import UserAvatarImg from "@/public/assets/UserAvatarImg.jpg"
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
-
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import Typography from '@mui/material/Typography';
 import Image from "next/image";
+import { grey } from '@mui/material/colors';
+import { Container, IconButton } from "@mui/material";
 
+const color = grey[900];
 
 export default function BusinessAvatar() {
   return (
-    <Box>
-
-      <Box display="flex"
-        justifyContent="space-around"
-      >
-        <Box display="flex" flexDirection="row"
-          sx={{ mr: 20, width: 160, height: 80 }}>
-          <Avatar
-            sx={{ width: 80, height: 80, ml: 4.75, mt: -2 }}
-          >
-            <Image
-              alt="profile picture"
-              src={UserAvatarImg}
-              placeholder="blur"
-              quality={100}
-              fill
-              sizes="100vw"
-              style={{
-                objectFit: 'cover',
-              }}
-            />
-          </Avatar>
-
-          <Box display="flex" flexDirection="column">
-            <Typography sx={{ fontWeight: 'bold' }}>
-              جلال شوقی        </Typography>
-            <Typography sx={{ fontWeight: 'bold' }}>
-              @Jalal.shoghi </Typography>
-          </Box>
-
-        </Box>
-        <Box
-
-          sx={{ width: 180 }}
+    <Container maxWidth="md">
+      <Box sx={{justifyContent: 'flex-start'}}  display="flex">
+        <Avatar
+          sx={{ width: 80, height: 80, mt: -5 }}
         >
-
-          <Typography display="inline" variant="subtitle2" sx={{ fontWeight: 'bold' }}>
-            سابقه فعالیت : 3 سال
-          </Typography>
-
-        </Box>
+          <Image
+            alt="profile picture"
+            src={UserAvatarImg}
+            placeholder="blur"
+            quality={100}
+            fill
+            sizes="100vw"
+            style={{
+              objectFit: 'cover',
+            }}
+          />
+        </Avatar>
+        <IconButton
+          sx={{mr:-2, bgcolor: color }}>
+          <AddAPhotoIcon sx={{ color: 'white' }} />
+        </IconButton>
       </Box>
-
-    </Box>
-
+    </Container>
   );
 }
