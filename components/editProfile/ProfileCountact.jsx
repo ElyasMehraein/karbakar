3d5029b7efcdf587b31f42cc160d008596dfea89
ElyasMehraein <elyas.mehraein@gmail.com>
2024-02-10 16:37:03 +0330

    Update ProfileCountact.jsx

--- components/editProfile/ProfileCountact.jsx
+++ components/editProfile/ProfileCountact.jsx
@@ -2,37 +2,63 @@ import React from 'react'
 import InstagramIcon from '@mui/icons-material/Instagram';
 import AddIcCallIcon from '@mui/icons-material/AddIcCall';
 import EmailIcon from '@mui/icons-material/Email';
+import WebIcon from '@mui/icons-material/Web';
+import ComputerIcon from '@mui/icons-material/Computer';
 import Box from '@mui/material/Box'
 import { Container, Typography } from '@mui/material';
+import TextField from '@mui/material/TextField';
+
 const phone = "09305845526"
+const email = "aa@bb.com"
+const personalPage = "google.com"
+const Instagram = "instagram.com"
+
 const ProfileCountact = () => {
     return (
         <Box>
             <Container maxWidth="md">
 
                 <Box
-                    sx={{ mb: 1 ,textDecoration:"none"}}
-
-                    display="flex"
-                    justifyContent="space-evenly"
+                    sx={{ mb: 1 }}
                 >
-                    <Box display="flex" flexDirection="column" align='center'>
-                        <Box><InstagramIcon fontSize="large" /></Box>
-                        <Box><Typography >اینستاگرام</Typography></Box>
+                    <Box display="flex" alignItems="center" align='center'>
+                        <Box><AddIcCallIcon fontSize="large" /></Box>
+                        <Box sx={{ width: '7ch', mx: 3 }}><Typography >تماس</Typography></Box>
+                        <TextField 
+                            id="outlined-helperText"
+                            label="مثال 09123456789"
+                        />
 
                     </Box>
-                    {/* < sx={{ pr: 2 }} display="flex" flexDirection="column" align='center'> */}
+                    <Box
+                        display="flex" alignItems="center" align='center' >
+                        <Box><EmailIcon fontSize="large" /></Box>
+                        <Box sx={{width: '7ch', mx: 3 }}><Typography >ایمیل</Typography></Box>
+                        <TextField
+                            id="outlined-helperText"
+                            label="مثال 09123456789"
+                        />
 
+                    </Box>
+                    <Box display="flex" alignItems="center" align='center'>
+                        <Box><ComputerIcon fontSize="large" /></Box>
+                        <Box sx={{ width: '7ch',mx: 3 }}><Typography >صفحه شخصی</Typography></Box>
+                        <TextField
+                            id="outlined-helperText"
+                            label="مثال 09123456789"
+                        />
 
-                    <a href={`tel:${phone}`}>
-                        <AddIcCallIcon fontSize="large" />
-                        <Typography >تماس</Typography>
-                    </a>
-                <Box sx={{ pr: 2 }} display="flex" flexDirection="column" align='center'>
-                    <Box><EmailIcon fontSize="large" /></Box>
-                    <Box><Typography >ایمیل</Typography></Box>
+                    </Box>
+                    <Box display="flex" alignItems="center" align='center'>
+                        <Box><InstagramIcon fontSize="large" /></Box>
+                        <Box sx={{width: '7ch', mx: 3 }}><Typography >اینستاگرام</Typography></Box>
+                        <TextField
+                            id="outlined-helperText"
+                            label="مثال 09123456789"
+                        />
+
+                    </Box>
                 </Box>
-        </Box >
             </Container >
         </Box >
     )
