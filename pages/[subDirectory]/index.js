75ee2dfde2991fa26d24fecaa29309e5ea58cf9c
ElyasMehraein <elyas.mehraein@gmail.com>
2024-02-13 20:04:01 +0330

    fixed some minor bugs so its the time for working on ui

--- pages/[subDirectory]/index.js
+++ pages/[subDirectory]/index.js
@@ -2,38 +2,26 @@ import Profile from '@/components/Profile/Profile'
 import connectToDB from '@/configs/db'
 import UserModel from '@/models/User'
 import BusinessModel from '@/models/Business'
-import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import Business from '@/components/business/business'
 
 export default function subDirectory(sub) {
   if (sub.user) {
-
     const whichUserProfile = sub.user.code
-    const router = useRouter()
-    const userCode = router.query.subDirectory
-    const [isLogedIn, setIsLogedIn] = useState(false)
     const [logedUserCode, setlogedUserCode] = useState(null)
-    const [isLogedInMyOwnProfile, setIsLogedInMyOwnProfile] = useState(false)
     useEffect(() => {
       const userAuth = async () => {
         const res = await fetch("/api/auth/me")
         if (res.status === 200) {
           const user = await res.json()
           setlogedUserCode(user.data.code)
-          setIsLogedIn(true)
         }
       }
       userAuth()
-      if (logedUserCode === whichUserProfile) {
-        setIsLogedInMyOwnProfile(true)
-      }
     }, [logedUserCode])
 
     return (
       <>
-        {/* <p>you are user {logedUserCode} </p>
-      <p>this is user {whichUserProfile} profile</p>
-      {isLogedInMyOwnProfile ? "page khodete" : "page shoma nist"} */}
         <Profile user={sub.user}
           logedUserCode={logedUserCode} whichUserProfile={whichUserProfile}
         />
@@ -41,25 +29,39 @@ export default function subDirectory(sub) {
 
     )
   } else if (sub.business) {
-    console.log("it is a bussiness");
+    return (
+      <>
+        <Business business={sub.business}
+          logedUserCode={logedUserCode} whichUserProfile={whichUserProfile}
+        />
+      </>
+
+    )
   }
 }
 
 export async function getServerSideProps(context) {
   connectToDB()
   if (isNaN(context.params.subDirectory)) {
+    console.log("it looks like a bussinessName => ", context.params.subDirectory);
     const business = await BusinessModel.findOne({ businessName: context.params.subDirectory })
-    if (!business) { return { notFound: true } }
+    if (!business) {
+      console.log("business not found in DB");
+      return { notFound: true }
+    }
     return {
       props: {
-        user: JSON.parse(JSON.stringify(business))
+        business: JSON.parse(JSON.stringify(business))
       }
     }
   }
   const user = await UserModel.findOne(
     { code: context.params.subDirectory },
   )
-  if (!user) { return { notFound: true } }
+  if (!user) {
+    console.log("user not found in DB");
+    return { notFound: true }
+  }
   return {
     props: {
       user: JSON.parse(JSON.stringify(user))
