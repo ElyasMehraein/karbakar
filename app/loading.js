eaee56efa1a21483b80cee93c1fa33b84848d629
ElyasMehraein <elyas.mehraein@gmail.com>
2024-02-23 14:38:20 +0330

    loading is beautifull ]

---
+++ app/loading.js
@@ -0,0 +1,19 @@
+import hands from "@/public/m-hands.png"
+import Image from "next/image";
+import styles from '@/styles/loading.module.css'
+
+export default function page() {
+  return (
+    <div className={styles.Loading}>
+      <header className={styles.loadnigHeader}>
+        <Image 
+          className={styles.LoadingLogo}
+          src={hands}
+          alt="karbakar loading logo"
+        />
+
+      </header>
+    </div>
+  );
+}
+
