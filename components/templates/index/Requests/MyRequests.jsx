d7f22481a650d0a897b1321d6e9c0a73ed60674f
ElyasMehraein <elyas.mehraein@gmail.com>
2024-04-22 22:22:07 +0330

    Update MyRequests.jsx

--- components/templates/index/Requests/MyRequests.jsx
+++ components/templates/index/Requests/MyRequests.jsx
@@ -1,33 +1,31 @@
-"user client"
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
 import YourRequestFrames from "./YourRequestFrames";
 import { mainTabYourReqText } from "@/components/typoRepo";
-import { Accordion, AccordionDetails, Chip, Container } from "@mui/material";
+import { Accordion, AccordionDetails, Chip, Container, Box, CircularProgress } from "@mui/material";
 import QuestionMarkOutlinedIcon from '@mui/icons-material/QuestionMarkOutlined';
-
 
 export default function MyRequests() {
   const [expanded, setExpanded] = React.useState(false);
-  const [requests, setRequests] = React.useState("")
+  const [requests, setRequests] = React.useState([]); // Use an empty array
+
   React.useEffect(() => {
     const getRequests = async () => {
       try {
-        const res = await fetch("/api/requests/myRequests/", { method: "GET" })
+        const res = await fetch("/api/requests/myRequests/", { method: "GET" });
         if (res.status === 200) {
-          const data = await res.json()
-          const requests = data.data
-          setRequests(requests)
+          const data = await res.json();
+          const requests = data.data;
+          setRequests(requests);
         }
       } catch (error) {
         console.error("Error fetching reports:", error);
       }
-    }
-    getRequests()
+    };
+    getRequests();
   }, []);
-  console.log("requests", requests);
 
   return (
     <>
@@ -42,17 +40,33 @@ export default function MyRequests() {
           {mainTabYourReqText}
         </AccordionDetails>
       </Accordion>
-      <Divider sx={{ fontWeight: 'bold' }} textAlign="center">درخواست های تایید شده</Divider>
-      <YourRequestFrames />
-      <YourRequestFrames />
-      <Divider sx={{ fontWeight: 'bold' }} textAlign="center">درخواست های منتظر تایید</Divider>
-      <YourRequestFrames />
-      <YourRequestFrames />
-      <YourRequestFrames />
-      <YourRequestFrames />
-      <YourRequestFrames />
-      <YourRequestFrames />
-      <YourRequestFrames />
+      {requests.length > 0 ? (
+        requests.map((request) => (
+          <Box key={request._id}>
+            {request.acceptedBy && (
+              <Divider sx={{ fontWeight: 'bold' }} textAlign="center">
+                درخواست های تایید شده
+              </Divider>
+            )}
+            {request.acceptedBy?.map((accepter) => (
+              <YourRequestFrames accepter={accepter} key={accepter} />
+            ))}
+
+            {request.needMoreInfo && (
+              <Divider sx={{ fontWeight: 'bold' }} textAlign="center">
+                درخواست های منتظر تایید
+              </Divider>
+            )}
+            {request.needMoreInfo?.map((accepter) => (
+              <YourRequestFrames accepter={accepter} key={accepter} />
+            ))}
+          </Box>
+        ))
+      ) : (
+        <Box className="inMiddle">
+          <CircularProgress />
+        </Box>
+      )}
     </>
   );
 }
