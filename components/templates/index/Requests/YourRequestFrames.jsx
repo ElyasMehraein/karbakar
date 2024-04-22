a78736800438f18e700cfd4f8443262a22102f62
ElyasMehraein <elyas.mehraein@gmail.com>
2024-04-23 02:28:13 +0330

    working on myRequests

--- components/templates/index/Requests/YourRequestFrames.jsx
+++ components/templates/index/Requests/YourRequestFrames.jsx
@@ -11,61 +11,68 @@ import CardHeader from "@mui/material/CardHeader";
 import { red } from "@mui/material/colors";
 import Box from "@mui/material/Box";
 import Image from 'next/image'
-import { Container } from "@mui/material";
+import { CardActionArea, Container } from "@mui/material";
 import ItsAvatar from "@/components/modules/ItsAvatar";
 import businessAvatar from "@/public/assets/businessAvatar.jpg";
+import ListItemButton from '@mui/material/ListItemButton';
+import SelectProvider from "./SelectProvider";
 
 const YourRequestFrames = ({ request }) => {
   return (
     <Container maxWidth="md">
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "flex-start",
-          flexDirection: "row",
-          justifyContent: "space-between"
-        }}
+      <CardActionArea
+      // onClick={}
       >
         <Box
           sx={{
             display: "flex",
             alignItems: "flex-start",
-            flexDirection: "column",
-            mr: 2
+            flexDirection: "row",
+            justifyContent: "space-between"
           }}
         >
-          <Typography sx={{ fontSize: 12, m: 0, fontWeight: 'bold' }} textAlign={"right"}>
-            {request.title}
-          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "flex-start",
+              flexDirection: "column",
+              mr: 2
+            }}
+
+          >
+            <Typography sx={{ fontSize: 12, m: 0, fontWeight: 'bold' }} textAlign={"right"}>
+              {request.title}
+            </Typography>
+          </Box>
+          <AvatarGroup dir="ltr" max={4}>
+            {request.acceptedBy?.map((acceptor) => {
+              return (
+                <Avatar key={acceptor}>
+                  <ItsAvatar isAvatar={acceptor.isAvatar} userCodeOrBusinessBrand={acceptor.businessName} alt="workers avatar" />
+                </Avatar>
+              )
+            })}
+            {request.needMoreInfo?.map((infoSeeker) => {
+              return (
+                <Avatar key={infoSeeker}>
+                  <ItsAvatar isAvatar={infoSeeker.isAvatar} userCodeOrBusinessBrand={infoSeeker.businessName} alt="workers avatar" />
+                </Avatar>
+              )
+            })}
+          </AvatarGroup>
         </Box>
-        <AvatarGroup dir="ltr" max={4}>
-          {request.acceptedBy?.map((acceptor) => {
-            return (
-              <Avatar key={acceptor}>
-                <ItsAvatar isAvatar={acceptor.isAvatar} userCodeOrBusinessBrand={acceptor.businessName} alt="workers avatar" />
-              </Avatar>
-            )
-          })}
-          {request.needMoreInfo?.map((infoSeeker) => {
-            return (
-              <Avatar key={infoSeeker}>
-                <ItsAvatar isAvatar={infoSeeker.isAvatar} userCodeOrBusinessBrand={infoSeeker.businessName} alt="workers avatar" />
-              </Avatar>
-            )
-          })}
-        </AvatarGroup>
-      </Box>
-      <Typography
-        paragraph
-        // noWrap
-        sx={{
-          mr: 2, fontSize: 11,
-          //later i deside if i need to use the below code
-          display: '-webkit-box',
-          overflow: 'hidden',
-          WebkitBoxOrient: 'vertical',
-          WebkitLineClamp: 2
-        }} align="justify" dir="rtl" >{request.message}</Typography>
+        <Typography
+          paragraph
+          // noWrap
+          sx={{
+            mr: 2, fontSize: 11,
+            //later i deside if i need to use the below code
+            display: '-webkit-box',
+            overflow: 'hidden',
+            WebkitBoxOrient: 'vertical',
+            WebkitLineClamp: 2
+          }} align="justify" dir="rtl" >{request.message}</Typography>
+      </CardActionArea>
 
     </Container>
   );
