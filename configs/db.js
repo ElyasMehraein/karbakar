88d1f31ff132c7cd7b9c04e94238b37b41f6afcc
ElyasMehraein <elyas.mehraein@gmail.com>
2024-04-26 13:44:46 +0330

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -1,12 +1,11 @@
 const { default: mongoose } = require("mongoose");
 
-export default connectToDB;
 
 const connectToDB = async () => {
   if (mongoose.connections[0].readyState) {
     return true;
   }
-
+  
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connect To DB Successfully :))");
@@ -15,3 +14,4 @@ const connectToDB = async () => {
     throw err;
   }
 };
+export default connectToDB;
