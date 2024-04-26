b8391df77c2a0e4a56e359e76972cd98daaf6c9a
ElyasMehraein <elyas.mehraein@gmail.com>
2024-04-26 23:04:49 +0330

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import React from 'react'
 
 export default function page() {
     return (
-        <Typography sx={{ height: "100vh", display:"flex" , justifyContent:"center" , alignContent:"center"}} className='inMiddle'>یه مشکل بزرگ پیدا کردم حلش دو سه روز طول می کشه توی این مدت مجبوریم صبر کنیم😅</Typography>
+        <Typography sx={{ p: 5, height: "100vh", display: "flex", justifyContent: "center", alignContent: "center" }} className='inMiddle'>
+            همون طور که میدونید تازه سایت راه اندازی شده و در مرحله تست هستیم حالا یه تعداد از باگ های سایت مشخص شده که حلش دو سه روز طول می کشه توی این مدت مجبوریم صبر کنیم😅
+        </Typography>
     )
 }
