1c68d4e6dbe426e1698619991b5a2f6ac3721562
ElyasMehraein <elyas.mehraein@gmail.com>
2024-04-29 23:19:21 +0330

    Update page.js
    test

--- app/all/page.js
+++ app/all/page.js
@@ -1,20 +1,20 @@
-import connectToDB from "@/configs/db";
-import BusinessModel from "@/models/Business";
-import AllBusinesses from "@/components/templates/allBusinesses/AllBusinesses";
+// import connectToDB from "@/configs/db";
+// import BusinessModel from "@/models/Business";
+// import AllBusinesses from "@/components/templates/allBusinesses/AllBusinesses";
 
 export default async function page() {
-  try {
-    await connectToDB();
-    const businesses = JSON.parse(JSON.stringify(await BusinessModel.find({}, "businessName bio businessBrand isAvatar")));
+  // try {
+  //   await connectToDB();
+  //   const businesses = JSON.parse(JSON.stringify(await BusinessModel.find({}, "businessName bio businessBrand isAvatar")));
 
 
     return (
       // <AllBusinesses businesses={businesses} />
       <h1 className="inMiddle">تست</h1>
     );
-  } catch (err) {
+  // } catch (err) {
     
-    console.error('Error fetching businesses:', err);
-  }
+  //   console.error('Error fetching businesses:', err);
+  // }
 }
 
