0b7bf89508b8412833026a9273e331cd02882235
ElyasMehraein <elyas.mehraein@gmail.com>
2024-04-30 18:11:25 +0330

    Update page.js

--- app/all/page.js
+++ app/all/page.js
@@ -4,9 +4,11 @@ import dynamic from 'next/dynamic'
 const AllBusinesses = dynamic(() => import("@/components/templates/allBusinesses/AllBusinesses"))
 
 const page = async function () {
-  try {
     await connectToDB();
-    const businesses = JSON.parse(JSON.stringify(await BusinessModel.find({}, "businessName bio businessBrand isAvatar")));
+    const businesses = JSON.parse(JSON.stringify(await BusinessModel.find({},
+       "businessName bio businessBrand isAvatar").catch(err => {
+        console.error(err);
+    })));
 
 
     return (
@@ -14,10 +16,8 @@ const page = async function () {
       // <h1 className="inMiddle">تست</h1>
 
     );
-  } catch (err) {
 
-    console.error('Error fetching businesses:', err);
-  }
+
 }
 
 export default page
