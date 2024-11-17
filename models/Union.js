1d8d7ddcb92519ab3cf91a9ea05b1797095f268a
ElyasMehraein <elyas.mehraein@gmail.com>
2024-11-17 13:21:53 +0330

    fix union story and model

--- models/Union.js
+++ models/Union.js
@@ -3,63 +3,24 @@ import { Schema } from "mongoose";
 
 const schema = new Schema({
     unionName: { type: String, required: true, maxlength: 40, minlength: 4, },
-    slogan: {
-        type: String,
-        maxlength: 150,
-    },
+    slogan: { type: String, maxlength: 150, },
     validityPeriod: { type: Number, required: true },
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Business',
-        required: true
-    },
-    members: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Business',
-        required: true
-    }],
+    extensionPeriod: { type: Number, required: true },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Business', required: true }],
     votes: [{
-        voter: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Business',
-            required: true
-        },
-        voteFor: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Business',
-            required: true
-        },
-        approved: {
-            type: Boolean,
-            required: true
-        }
+        voter: { type: mongoose.Schema.Types.ObjectId, ref: 'Business' },
+        voteFor: { type: mongoose.Schema.Types.ObjectId, ref: 'Business' },
+    }],
+    extensionVote: [{
+        extensionVoter: { type: mongoose.Schema.Types.ObjectId, ref: 'Business' },
     }],
     transactions: [{
-        provider: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Business',
-            required: true
-        },
-        recipient: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Business',
-            required: true
-        },
-        product: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Product',
-            required: true
-        },
-        quantity: {
-            type: Number,
-            required: true
-        }
+        provider: { type: mongoose.Schema.Types.ObjectId, ref: 'Business' },
+        recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'Business' },
+        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+        quantity: { type: Number, required: true }
     }],
-    status: {
-        type: String,
-        enum: ['pending', 'active'],
-        default: 'pending'
-    }
+    isActive: { type: Boolean, }
 }, { timestamps: true });
 
 const UnionModel = mongoose.models.Union || mongoose.model("Union", schema);
