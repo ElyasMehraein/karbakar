17a09521f8e9d8e054448c2034dc418484c65220
ElyasMehraein <elyas.mehraein@gmail.com>
2024-12-29 23:22:35 +0330

    working on joining union

--- components/templates/index/ThirdTab/components/ThirdTabAccordion.jsx
+++ components/templates/index/ThirdTab/components/ThirdTabAccordion.jsx
@@ -7,80 +7,86 @@ import { blue } from '@mui/material/colors';
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { Button } from '@mui/material';
+import ThirdTabjoinAUnion from './ThirdTabjoinAUnion';
 
 export default function ThirdTabAccordion({ union, accordionDetails, primeBusiness, user }) {
 
   const [open, setOpen] = useState(false);
   const handleMembership = () => {
-    setOpen(true);
+    // setOpen(true);
+    console.log("ha biua");
+
   };
 
   return (
-    <Accordion
-      disableGutters
-      sx={{ bgcolor: blue[50], my: 1, minWidth: 300, width: '100%' }}
-    >
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon sx={{ alignSelf: 'flex-start' }} />}
-        aria-controls="pane-content"
-        id="pane-header"
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'flex-start',
-          pl: 1,
-          minHeight: 56,
-          position: 'relative',
-        }}
+    <React.Fragment>
+      <ThirdTabjoinAUnion  {...{ primeBusiness, user,union }} />
+      <Accordion
+        disableGutters
+        sx={{ bgcolor: blue[50], my: 1, minWidth: 300, width: '100%' }}
       >
-        <Box
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon sx={{ alignSelf: 'flex-start' }} />}
+          aria-controls="pane-content"
+          id="pane-header"
           sx={{
             display: 'flex',
-            alignItems: 'flex-start',
-            flexDirection: 'column',
-          }}
-        >
-          <Typography
-            sx={{ fontSize: 12, m: 0, fontWeight: 'bold' }}
-            textAlign="right"
-          >
-            {union.unionName}
-          </Typography>
-          <Typography
-            sx={{
-              fontSize: 11,
-              overflow: 'hidden',
-              WebkitBoxOrient: 'vertical',
-              WebkitLineClamp: 2,
-            }}
-            align="justify"
-            dir="rtl"
-          >
-            {union.slogan}
-          </Typography>
-        </Box>
-      </AccordionSummary>
-
-      {accordionDetails(union)}
-
-      <AccordionActions>
-        <Box
-          sx={{
-            display: 'flex',
+            flexDirection: 'row',
             justifyContent: 'space-between',
-            alignItems: 'center',
-            width: '100%',
+            alignItems: 'flex-start',
+            pl: 1,
+            minHeight: 56,
+            position: 'relative',
           }}
         >
-          <Typography sx={{ mr: 1, fontSize: '12px' }}>
-            {`مدت اتحاد: ${union.deadline} روز`}
-          </Typography>
-          <Button variant="contained" color="primary" onClick={handleMembership}>
-            عضویت
-          </Button>
-        </Box>
-      </AccordionActions>
-    </Accordion>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'flex-start',
+              flexDirection: 'column',
+            }}
+          >
+            <Typography
+              sx={{ fontSize: 12, m: 0, fontWeight: 'bold' }}
+              textAlign="right"
+            >
+              {union.unionName}
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: 11,
+                overflow: 'hidden',
+                WebkitBoxOrient: 'vertical',
+                WebkitLineClamp: 2,
+              }}
+              align="justify"
+              dir="rtl"
+            >
+              {union.slogan}
+            </Typography>
+          </Box>
+        </AccordionSummary>
+
+        {accordionDetails(union)}
+
+        <AccordionActions>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: '100%',
+            }}
+          >
+            <Typography sx={{ mr: 1, fontSize: '12px' }}>
+              {`مدت اتحاد: ${union.deadline} روز`}
+            </Typography>
+            <Button variant="contained" color="primary" onClick={handleMembership}>
+              عضویت
+            </Button>
+          </Box>
+        </AccordionActions>
+      </Accordion>
+    </React.Fragment>
   );
 }
--- components/templates/index/ThirdTab/components/ThirdTabjoinAUnion.jsx
+++ components/templates/index/ThirdTab/components/ThirdTabjoinAUnion.jsx
@@ -1,50 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
 import {
     Button,
     Container,
-    Divider,
     FormControl,
-    Paper,
-    useMediaQuery,
+    Slide,
 } from "@mui/material";
-import ItsAvatar from "@/components/modules/ItsAvatar";
-import Accordion from '@mui/material/Accordion';
-import AccordionActions from '@mui/material/AccordionActions';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { blue } from '@mui/material/colors';
-import { useRouter } from "next/navigation";
-import { useState } from "react";
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
 import BasketSelection from '@/components/modules/BasketSelection';
 import SelectOrCreateCategoryAndGuild from '@/components/modules/SelectOrCreateCategoryAndGuild';
 import {
     InputLabel,
     MenuItem,
     Select,
-    Table,
-    TableHead,
-    TableBody,
-    TableRow,
-    TableCell,
-    TableContainer,
 } from '@mui/material';
 
-export default function ThirdTabjoinAUnion() {
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function ThirdTabjoinAUnion({ union, primeBusiness, user }) {
+
+    const [selectedBusinessName, setSelectedBusinessName] = useState(primeBusiness.businessName);
+    const userBusinesses = user.businesses.map((business) => business.businessName);
+    const selectedBusiness = user.businesses.find((business) => {
+        if (business.businessName === selectedBusinessName) {
+            return business;
+        }
+    });
+    // سبدهای عرضه و تقاضا
+    const [offerBasket, setOfferBasket] = useState([]);
+    const [demandBasket, setDemandBasket] = useState([]);
+
+    const addOfferBasket = (value) => {
+        setOfferBasket(value);
+    };
+    const addDemandBasket = (value) => {
+        setDemandBasket(value);
+    };
+
+    // دسته، صنف و ...
+    const [demandGuild, setDemandGuild] = useState(null);
+    const [demandGuildName, setDemandGuildName] = useState(null);
+    const [demandJobCategory, setDemandJobCategory] = useState(null);
+
+    const getDataFromChild = (guild, guildName, jobCategory) => {
+        setDemandGuild(guild);
+        setDemandGuildName(guildName);
+        setDemandJobCategory(jobCategory);
+    };
+
+    // عضویت در اتحاد
+    async function joinAUnion() {
+        const res = await fetch('api/joinAUnion', {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                unionID: union._id,
+                businessID: selectedBusiness._id,
+                offerBasket,
+                demandBasket,
+                guildID: demandGuild?._id,
+                guildName: demandGuildName,
+                jobCategory: demandJobCategory
+            })
+        });
+        if (res.status === 500) {
+            console.log("server error");
+        } else if (res.status === 409) {
+            // ...
+        } else if (res.status === 201) {
+            console.log("joined union successfully", res);
+            // ریست مقادیر
+            setOfferBasket([]);
+            setDemandBasket([]);
+        }
+    }
+
     return (
         <Dialog
-            open={open}
+            // open={open}
             TransitionComponent={Transition}
             keepMounted
-            onClose={handleClose}
+        // onClose={handleClose}
         >
             <DialogTitle>عضویت در اتحاد</DialogTitle>
             <DialogContent>
@@ -87,9 +128,9 @@ export default function ThirdTabjoinAUnion() {
                 </Container>
             </DialogContent>
             <DialogActions>
-                <Button sx={{ m: 2 }} color="info" variant="contained" onClick={handleClose}>
+                {/* <Button sx={{ m: 2 }} color="info" variant="contained" onClick={handleClose}>
                     بازگشت
-                </Button>
+                </Button> */}
                 <Button
                     children={"عضویت در اتحاد"}
                     variant="contained"
