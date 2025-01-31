0343d00f78c448ece132a84fa3170b1d63718e76
ElyasMehraein <elyas.mehraein@gmail.com>
2025-01-31 21:50:27 +0330

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,9 +10,8 @@ export default function RootLayout({ children }) {
       <head>
         <title>کارباکار</title>
         <meta
-          // name="description"
-          // content="اقتصاد اجتماعی غیر پولی برای مبادله بدون واسطه محصولات و خدمات افراد و کسب و کارهای مولد"
-          name="enamad" content="46997260"
+          name="description"
+          content="اقتصاد اجتماعی غیر پولی برای مبادله بدون واسطه محصولات و خدمات افراد و کسب و کارهای مولد"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
